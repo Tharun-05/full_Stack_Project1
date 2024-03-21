0cc983232254981169c19b481b59d1accf6ca352
Tharun-05 <106149950+Tharun-05@users.noreply.github.com>
2024-03-21 16:43:47 +0530

    Update db.config.js

--- Nodejs/Config/db.config.js
+++ Nodejs/Config/db.config.js
@@ -1,2 +1,2 @@
 
-module.exports ={url:"mongodb+srv://Tharunkumar:CRAZYboy%4005@cluster0.nxivib9.mongodb.net/",};
+module.exports ={url:"mongodb+srv://Tharunkumar:CRAZYboy%4005@cluster0.nxivib9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",};
