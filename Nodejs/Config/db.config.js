41e6a763d64e3b8b88b2f2c3286936b9b8fb7aeb
Tharun-05 <tharunkumar188466@gmail.com>
2024-03-22 09:29:20 +0530

    Nodejs config updated

--- Nodejs/Config/db.config.js
+++ Nodejs/Config/db.config.js
@@ -1,18 +1,1 @@
-
 module.exports ={url:"mongodb+srv://Tharunkumar:CRAZYboy%4005@cluster0.nxivib9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",};
-<<<<<<< HEAD
-// const mongoose=require("mongoose");
-// mongoose.set("strictQuery",false);
-// const connectDB=async()=>{
-//     try{
-//         const conn=await mongoose.connect(process.env.MONGO_URL);
-//         console.log('MongoDB Connected: ${conn.connection.host}');
-
-//     }catch(error){
-// console.log(error);
-// process.exit(1);
-//     }
-// };
-// module.exports=connectDB;
-=======
->>>>>>> ee55b03c2c574fd88fefe3ae1f20e8eef94d2ae1
