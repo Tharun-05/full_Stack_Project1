4481d9fb5d30306d83f2ad05b19eba4c8dca7849
Tharun-05 <tharunkumar188466@gmail.com>
2024-03-22 12:08:22 +0530

    Nodejs

--- Nodejs/Models/Restaurents.model.js
+++ Nodejs/Models/Restaurents.model.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 // const {Schema}=mongoose;
 const RestaurentSchema=mongoose.Schema({
     name:String,
-    cuisines:[String],
+    cuisines:Array,
     locality:String,
     areaName:String,
     costForTwo:String,
